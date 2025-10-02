845636faab598fbebba6ef3961e09bacb275e15d
Andrei Spelman <andrei.spelman@aalto.fi>
2025-10-02 13:49:09 +0300

    Almost workig swap

--- src/main/scala/clkdiv_universal/clkdiv_universal.scala
+++ src/main/scala/clkdiv_universal/clkdiv_universal.scala
@@ -27,9 +27,9 @@ class clkdiv_universalIO(n: Int, word_res: Int=32, out_res: Int=16) extends Bund
         val clkpf2n     = Output(Bool())
         val clkpf4n     = Output(Bool())
         val clkpf8n     = Output(Bool())
-        val clkpf       = Output(Bool())
+        val clkpn       = Output(Bool())
         val clk_slowest = Output(Bool())
-        val clkp1_sync  = Output(Bool())
+        val clkpn_sync  = Output(Bool())
         val phase       = Output(SInt((out_res).W))
     }
 }
@@ -39,18 +39,52 @@ class clkdiv_universal (n: Int=8, word_res: Int=32, out_res: Int=16) extends Mod
 
     val en = Wire(Bool()) 
     en := !io.control.reset_clk 
+    
+    val r_shift        = RegInit(0.U.asTypeOf(io.control.shift))
+    val r_Ndiv         = RegInit(1.U.asTypeOf(io.control.Ndiv))
+    //Sync the shift
+    r_shift := io.control.shift
+
+    //Sync the Ndiv
+    r_Ndiv := io.control.Ndiv
+
+    val div_n=RegInit(false.B)
+    val count          = RegInit(0.U(n.W))
+    val phase_reset_primary =RegInit(true.B)
+    val phase_reset_secondary =RegInit(true.B)
+    phase_reset_secondary := ShiftRegister(phase_reset_primary, 5, true.B, true.B)
+    when (en) {
+        when (count >= r_Ndiv - 1) {
+            count := 0.U
+            div_n := true.B
+            phase_reset_primary := false.B
+        } .otherwise {
+            count := count + 1.U(1.W)
+            div_n := false.B
+            phase_reset_primary :=phase_reset_primary
+        }
+    }
+
+    
+    val neg_clock = (!(clock.asBool)).asClock
+    val clk_div_n_mux= Wire(Bool())
+    val use_div_n = withClock(neg_clock){RegInit(false.B)}
+    use_div_n :=withClock(neg_clock){r_shift >= 1.U  }
+    clk_div_n_mux := Mux(use_div_n.asBool, clock.asBool, div_n)
+    io.out.clkpn  := clk_div_n_mux
+
 
     // Phase accum and register for mu
-    val phaseaccum = Module(new phaseaccum(word_res=word_res,out_res=out_res))
-    phaseaccum.io.control.word     := io.control.word
-    phaseaccum.io.control.word_mu  := io.control.word_mu
-    phaseaccum.io.control.convmode := io.control.convmode
+      val phase_reset =RegInit(true.B)
+      val phaseaccum =  withClockAndReset(clk_div_n_mux.asClock, phase_reset_secondary){Module(new phaseaccum(word_res=word_res,out_res=out_res)) }
+      phaseaccum.io.control.word     := io.control.word
+      phaseaccum.io.control.word_mu  := io.control.word_mu
+      phaseaccum.io.control.convmode := io.control.convmode
 
-    io.out.clkp1_sync  := phaseaccum.io.out.clkp1_sync
-    io.out.phase := phaseaccum.io.out.phase
+      io.out.clkpn_sync  := phaseaccum.io.out.clkp1_sync
+      io.out.phase := phaseaccum.io.out.phase
 
 
-    val neg_clock = (!(clock.asBool)).asClock
     val enab_frac            = withClock(neg_clock){RegInit(0.U(1.W))}
     val convmode_switch_async= withClock(neg_clock){RegInit(0.U(1.W))}
 
@@ -58,172 +92,155 @@ class clkdiv_universal (n: Int=8, word_res: Int=32, out_res: Int=16) extends Mod
     enab_frac           := withClock(neg_clock){io.control.word =/= 0.U}
     val clk_div_master_mux= Wire(Bool())
     clk_div_master_mux := Mux(enab_frac.asBool, phaseaccum.io.out.clkpf, clock.asBool)
-    io.out.clkpf  := clk_div_master_mux
+    io.out.clkpfn  := clk_div_master_mux
 
-    
+        
     withClock(clk_div_master_mux.asClock){
-    val r_shift        = RegInit(0.U.asTypeOf(io.control.shift))
-    val r_Ndiv         = RegInit(1.U.asTypeOf(io.control.Ndiv))
-    val stateregisters = RegInit(VecInit(Seq.fill(4)(false.B)))
+      val stateregisters = RegInit(VecInit(Seq.fill(3)(false.B)))
 
-    //Sync the shift
-    r_shift := io.control.shift
+      
 
-    //Sync the Ndiv
-    r_Ndiv := io.control.Ndiv
+      val enN = RegInit(false.B) 
+      enN := en
 
-    val count          = RegInit(0.U(n.W))
-    when (en) {
-        when (count >= r_Ndiv - 1) {
-            count := 0.U
-            stateregisters(0) := true.B
-        } .otherwise {
-            count := count + 1.U(1.W)
-            stateregisters(0) := false.B
-        }
-    }
-    
-
-    val enN = RegInit(false.B) 
-    enN := en
-
-    //Enable registers. We need to delay the enable by one clock pulse in order
-    // to get the feedbacks reseted
-    val en2 = RegInit(false.B)
-    val en4 = RegInit(false.B)
-    val en8 = RegInit(false.B)
-
-    //Chaining the enables
-    when (stateregisters(0)){ 
-        en2 := (enN &&  en )
-    }
-    when (stateregisters(1) ){
-        en4 := en2
-    }
-    when (stateregisters(2) ){
-        en8 := en4
-    }
-    
-    val enchain = Seq(enN, en2, en4, en8)
+      //Enable registers. We need to delay the enable by one clock pulse in order
+      // to get the feedbacks reseted
+      val en2 = RegInit(false.B)
+      val en4 = RegInit(false.B)
+      val en8 = RegInit(false.B)
+      
+      stateregisters(0) := ~stateregisters(0)
+      //Chaining the enables
+      when (stateregisters(0)){ 
+          en2 := (enN &&  en )
+      }
+      when (stateregisters(1) ){
+          en4 := en2
+      }
+      when (stateregisters(2) ){
+          en8 := en4
+      }
+      
+      val enchain = Seq(enN, en2, en4, en8)
 
 
-    // Monitors if the all previous stages are zero
-    val allzp = Wire(Vec(4,Bool()))
-    allzp(0) :=  stateregisters(0) 
+      // Monitors if the all previous stages are zero
+      val allzp = Wire(Vec(3,Bool()))
+      allzp(0) :=  stateregisters(0) 
 
-    for ( i <- 1 to 3) {
-        allzp(i) := allzp(i - 1) && !stateregisters(i)
-    }
+      for ( i <- 1 to 2) {
+          allzp(i) := allzp(i - 1) && !stateregisters(i)
+      }
 
-    val outregs = RegInit(VecInit(Seq.fill(4)(false.B)))
-    
-    outregs(0) := stateregisters(0)
+      val outregs = RegInit(VecInit(Seq.fill(3)(false.B)))
+      
+      outregs(0) := stateregisters(0)
 
-    for ( i <- 1 to 3) {
-       when (en) { 
-           when ((enchain(i) && allzp(i - 1))) {
-              stateregisters(i) := ! stateregisters(i)
-           } .otherwise { 
-               stateregisters(i) := stateregisters(i)
-           }
-       } .otherwise { 
-           stateregisters(i) := false.B  
-     }
-     //Pure registers at the output
-     outregs(i) := stateregisters(i)
-    }
+      for ( i <- 1 to 2) {
+         when (en) { 
+             when ((enchain(i) && allzp(i - 1))) {
+                stateregisters(i) := ! stateregisters(i)
+             } .otherwise { 
+                 stateregisters(i) := stateregisters(i)
+             }
+         } .otherwise { 
+             stateregisters(i) := false.B  
+       }
+       //Pure registers at the output
+       outregs(i) := stateregisters(i)
+      }
 
-    
-    //First we sync or zero the divided clocks depending on the shift 
-    val syncregs = RegInit(VecInit(Seq.fill(4)(false.B)))
-    val w_clkpn = Wire(Bool())
+      
+      //First we sync or zero the divided clocks depending on the shift 
+      val syncregs = RegInit(VecInit(Seq.fill(3)(false.B)))
+      val w_clkpn = Wire(Bool())
   
-    //Shifting mux
-    w_clkpn := RegNext(outregs(0))
-    when (r_shift === 0.U){
-        syncregs(0) := w_clkpn
-        syncregs(1) := outregs(1) 
-        syncregs(2) := outregs(2)
-        syncregs(3) := outregs(3)
-    }.elsewhen(r_shift - 1 === 0.U){
-        syncregs(0) := 0.U;
-        syncregs(1) := w_clkpn 
-        syncregs(2) := outregs(1)
-        syncregs(3) := outregs(2)
-    }.elsewhen(r_shift - 2 === 0.U){
-        syncregs(0) := 0.U
-        syncregs(1) := 0.U
-        syncregs(2) := w_clkpn
-        syncregs(3) := outregs(1)
-    }.elsewhen(r_shift - 3 === 0.U){
-        syncregs(0) := 0.U
-        syncregs(1) := 0.U 
-        syncregs(2) := 0.U
-        syncregs(3) := w_clkpn
-    }.elsewhen(r_shift - 4 === 0.U){
-        syncregs(0) := 0.U
-        syncregs(1) := 0.U 
-        syncregs(2) := 0.U
-        syncregs(3) := 0.U
-    }.otherwise{
-        syncregs(0) := w_clkpn
-        syncregs(1) := outregs(1) 
-        syncregs(2) := outregs(2)
-        syncregs(3) := outregs(3)
-    }
+      //Shifting mux
+      w_clkpn := RegNext(outregs(0))
+      when (r_shift === 0.U){
+          syncregs(0) := w_clkpn
+          syncregs(1) := outregs(1) 
+          syncregs(2) := outregs(2)
+          //syncregs(3) := outregs(3)
+      }.elsewhen(r_shift - 1 === 0.U){
+          syncregs(0) := w_clkpn
+          syncregs(1) := outregs(1) 
+          syncregs(2) := outregs(2)
+          //syncregs(3) := outregs(2)
+      }.elsewhen(r_shift - 2 === 0.U){
+          syncregs(0) := 0.U;
+          syncregs(1) := w_clkpn 
+          syncregs(2) := outregs(1)
+          //syncregs(3) := outregs(1)
+      }.elsewhen(r_shift - 3 === 0.U){
+          syncregs(0) := 0.U
+          syncregs(1) := 0.U
+          syncregs(2) := w_clkpn
+          //syncregs(3) := w_clkpn
+      }.elsewhen(r_shift - 4 === 0.U){
+          syncregs(0) := 0.U
+          syncregs(1) := 0.U 
+          syncregs(2) := 0.U
+          //syncregs(3) := 0.U
+      }.otherwise{
+          syncregs(0) := w_clkpn
+          syncregs(1) := outregs(1) 
+          syncregs(2) := outregs(2)
+          //syncregs(3) := outregs(3)
+      }
 
 
-    // Output selection logic
-    val w_isdivone = Wire(Bool())
-    w_isdivone := (r_Ndiv - 1 === 0.U)
+      // Output selection logic
+      val w_isdivone = Wire(Bool())
+      w_isdivone := (r_Ndiv - 1 === 0.U)
 
-    val w_sel1_clock_clkpfn  = Wire(Bool())
-    val w_sel1_clock_clkpf2n = Wire(Bool())
-    val w_sel1_clock_clkpf4n = Wire(Bool())
-    val w_sel1_clock_clkpf8n = Wire(Bool())
-    val w_seln_clock_clkpfn  = Wire(Bool())
-    val w_seln_clock_clkpf2n = Wire(Bool())
-    val w_seln_clock_clkpf4n = Wire(Bool())
-    val w_seln_clock_clkpf8n = Wire(Bool()) 
+      val w_sel1_clock_clkpfn  = Wire(Bool())
+      val w_sel1_clock_clkpf2n = Wire(Bool())
+      val w_sel1_clock_clkpf4n = Wire(Bool())
+      val w_sel1_clock_clkpf8n = Wire(Bool())
+      val w_seln_clock_clkpfn  = Wire(Bool())
+      val w_seln_clock_clkpf2n = Wire(Bool())
+      val w_seln_clock_clkpf4n = Wire(Bool())
+      val w_seln_clock_clkpf8n = Wire(Bool()) 
 
-    //Selector signals for the output mux
-    w_sel1_clock_clkpfn  := w_isdivone && ((r_shift === 0.U))
-    w_sel1_clock_clkpf2n := w_isdivone && ((r_shift === 0.U) || (r_shift - 1 === 0.U))
-    w_sel1_clock_clkpf4n := w_isdivone && ((r_shift === 0.U) || (r_shift - 1 === 0.U) || (r_shift - 2 === 0.U))
-    w_sel1_clock_clkpf8n := w_isdivone && ((r_shift === 0.U) || (r_shift - 1 === 0.U) || (r_shift - 2 === 0.U) || (r_shift - 3 === 0.U))
-    
-    w_seln_clock_clkpfn  := ((r_shift - 1 === 0.U))
-    w_seln_clock_clkpf2n := ((r_shift - 2 === 0.U))
-    w_seln_clock_clkpf4n := ((r_shift - 3 === 0.U))
-    w_seln_clock_clkpf8n := ((r_shift - 4 === 0.U))
+      //Selector signals for the output mux
+      w_sel1_clock_clkpfn  := w_isdivone && ((r_shift === 0.U))
+      w_sel1_clock_clkpf2n := w_isdivone && ((r_shift === 0.U) || (r_shift - 1 === 0.U))
+      w_sel1_clock_clkpf4n := w_isdivone && ((r_shift === 0.U) || (r_shift - 1 === 0.U) || (r_shift - 2 === 0.U))
+      w_sel1_clock_clkpf8n := w_isdivone && ((r_shift === 0.U) || (r_shift - 1 === 0.U) || (r_shift - 2 === 0.U) || (r_shift - 3 === 0.U))
+      
+      w_seln_clock_clkpfn  := ((r_shift - 1 === 0.U))
+      w_seln_clock_clkpf2n := ((r_shift - 2 === 0.U))
+      w_seln_clock_clkpf4n := ((r_shift - 3 === 0.U))
+      w_seln_clock_clkpf8n := ((r_shift - 4 === 0.U))
 
-    // Output Muxes
-    //Mux for clkpfn
-    when (w_sel1_clock_clkpfn || w_seln_clock_clkpfn){
-        io.out.clkpfn := clk_div_master_mux.asUInt
-    } .otherwise {
-        io.out.clkpfn := syncregs(0)
-    }
-    //Mux for clkp2n
-    when (w_sel1_clock_clkpf2n || w_seln_clock_clkpf2n){
-        io.out.clkpf2n := clk_div_master_mux.asUInt
-    } .otherwise {
-        io.out.clkpf2n := syncregs(1)
-    }
-    //Mux for clkp4n
-    when (w_sel1_clock_clkpf4n || w_seln_clock_clkpf4n){
-        io.out.clkpf4n := clk_div_master_mux.asUInt
-    } .otherwise {
-        io.out.clkpf4n := syncregs(2)
-    }
-    //Mux for clkp8n
-    when (w_sel1_clock_clkpf8n || w_seln_clock_clkpf8n){
-        io.out.clkpf8n := clk_div_master_mux.asUInt
-    } .otherwise {
-        io.out.clkpf8n := syncregs(3)
-    }
+      // Output Muxes
+      //Mux for clkpfn
+      when (w_sel1_clock_clkpfn || w_seln_clock_clkpfn){
+          io.out.clkpfn := clk_div_master_mux.asUInt
+      } .otherwise {
+          io.out.clkpfn := clk_div_master_mux
+      }
+      //Mux for clkp2n
+      when (w_sel1_clock_clkpf2n || w_seln_clock_clkpf2n){
+          io.out.clkpf2n := clk_div_master_mux.asUInt
+      } .otherwise {
+          io.out.clkpf2n := syncregs(0)
+      }
+      //Mux for clkp4n
+      when (w_sel1_clock_clkpf4n || w_seln_clock_clkpf4n){
+          io.out.clkpf4n := clk_div_master_mux.asUInt
+      } .otherwise {
+          io.out.clkpf4n := syncregs(1)
+      }
+      //Mux for clkp8n
+      when (w_sel1_clock_clkpf8n || w_seln_clock_clkpf8n){
+          io.out.clkpf8n := clk_div_master_mux.asUInt
+      } .otherwise {
+          io.out.clkpf8n := syncregs(2)
+      }
 
-     io.out.clk_slowest := syncregs(3)
+       io.out.clk_slowest := syncregs(2)
   }
 }
 
@@ -265,7 +282,6 @@ class phaseaccum (word_res: Int=32, out_res: Int=16) extends Module {
   when(io.control.convmode === 1.U){
     enab2_re := ShiftRegister(enab2 & !ShiftRegister(enab2, 1, 0.U, true.B), 2, 0.U, true.B)
   }.otherwise {
-    //enab2_re := ShiftRegister(enab2 & !ShiftRegister(enab2, 1, 0.U, true.B), 1, 0.U, true.B)
     enab2_re := enab2 & !ShiftRegister(enab2, 1, 0.U, true.B)
   }
   
@@ -291,7 +307,6 @@ class phaseaccum (word_res: Int=32, out_res: Int=16) extends Module {
     io.out.clkpf      := clock.asUInt & enab
     io.out.clkp1_sync := clock.asUInt & enab_clk_sync
     out_reg           := accum((word_res-1), (word_res-out_res+1)).zext
-    //io.out.phase      := ShiftRegister(out_reg, 1, 0.S, true.B)  
     io.out.phase      := out_reg
   }
 }
